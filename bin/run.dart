1eacc313a190d37bb6371fb72238e075bbe349f6
Matthew Butler <butler.matthew@gmail.com>
2016-04-18 16:33:10 -0300

    Restructured default link.
    Removed old nodes until they can be checked.
    Added AddSiteNode.
    Added default node of 'Sites' with add side command.

--- bin/run.dart
+++ bin/run.dart
@@ -1,28 +1,37 @@
 import 'dart:async';
 
 import 'package:dslink/dslink.dart';
-import 'package:dslink_zoneminder/zoneminder_nodes.dart';
+
+import 'package:dslink_zoneminder/nodes.dart';
 
 Future main(List<String> args) async {
   LinkProvider link;
 
-  link = new LinkProvider(args, 'ZoneMinder-',
-      command: 'run',
-      profiles: {
-        GetMonitors.isType: (String path) => new GetMonitors(path, link),
-        ClearMonitors.isType: (String path) => new ClearMonitors(path, link),
-        MonitorNode.isType: (String path) => new MonitorNode(path),
-        MonitorValueNode.isType: (String path) => new MonitorValueNode(path),
-        MonitorStreamNode.isType: (String path) => new MonitorStreamNode(path),
-        MonitorEventsNode.isType: (String path) =>
-            new MonitorEventsNode(path, link),
-        EventValueNode.isType: (String path) => new EventValueNode(path),
-        EventStreamNode.isType: (String path) => new EventStreamNode(path)
-      },
-      autoInitialize: false);
+  link = new LinkProvider(args, 'ZoneMinder-', profiles: {
+        AddSiteNode.isType: (String path) => new AddSiteNode(path)
+      }, defaultNodes: {
+        'Sites' : {
+          AddSiteNode.pathName: AddSiteNode.definition()
+        }
+  });
 
-  link.init();
-  link.addNode('/${GetMonitors.pathName}', GetMonitors.definition());
-  link.addNode('/${ClearMonitors.pathName}', ClearMonitors.definition());
+//  link = new LinkProvider(args, 'ZoneMinder-',
+//      command: 'run',
+//      profiles: {
+//        GetMonitors.isType: (String path) => new GetMonitors(path, link),
+//        ClearMonitors.isType: (String path) => new ClearMonitors(path, link),
+//        MonitorNode.isType: (String path) => new MonitorNode(path),
+//        MonitorValueNode.isType: (String path) => new MonitorValueNode(path),
+//        MonitorStreamNode.isType: (String path) => new MonitorStreamNode(path),
+//        MonitorEventsNode.isType: (String path) =>
+//            new MonitorEventsNode(path, link),
+//        EventValueNode.isType: (String path) => new EventValueNode(path),
+//        EventStreamNode.isType: (String path) => new EventStreamNode(path)
+//      },
+//      autoInitialize: false);
+//
+//  link.init();
+//  link.addNode('/${GetMonitors.pathName}', GetMonitors.definition());
+//  link.addNode('/${ClearMonitors.pathName}', ClearMonitors.definition());
   await link.connect();
 }
